69d48ca8e2358b56e41cd759fb76a00d2f838d30
DESKTOP-J72R8IF\asus <trangnguyen2809>
2020-10-19 21:32:59 +0700

    no message

---
+++ app/src/main/java/com/example/fptparkingproject/model/Newfeed.java
@@ -0,0 +1,4 @@
+package com.example.fptparkingproject.model;
+
+public class Newfeed {
+}
---
+++ app/src/main/java/com/example/fptparkingproject/newfeed/newfeedHeaderActivity.java
@@ -0,0 +1,13 @@
+package com.example.fptparkingproject.newfeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newfeedHeaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newfeed_header);
+    }
+}
