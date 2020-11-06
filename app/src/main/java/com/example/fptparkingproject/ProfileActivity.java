a803fddc929ae8b3bc60e7dc89087368ab32d05a
DESKTOP-12NINI3\maihu <huongptmhe130039@fpt.edu.vn>
2020-11-06 10:43:39 +0700

    fix profile

--- app/src/main/java/com/example/fptparkingproject/ProfileActivity.java
+++ app/src/main/java/com/example/fptparkingproject/ProfileActivity.java
@@ -1,28 +1,14 @@
 package com.example.fptparkingproject;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.View;
-
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.fptparkingproject.signin.SignInWithGoogle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 
 public class ProfileActivity extends AppCompatActivity {
-// Xong rồi đấy
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +22,5 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 }
-
 
 
