0902edbb56593d536f37097249847dd28cd2d109
DESKTOP-LNI7OOO\Admin <ngahtthe130027@fpt.edu.vn>
2020-11-10 15:53:47 +0700

    feedback giao diện thiếu scrollview của 2 màn list feedback user và admin

--- app/src/main/java/com/example/fptparkingproject/customadapter/ViewFeedBackAdminAdapter.java
+++ app/src/main/java/com/example/fptparkingproject/customadapter/ViewFeedBackAdminAdapter.java
@@ -2,9 +2,11 @@ package com.example.fptparkingproject.customadapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +15,7 @@ import com.example.fptparkingproject.R;
 import com.example.fptparkingproject.constant.Constant;
 import com.example.fptparkingproject.model.Feedback;
 import com.example.fptparkingproject.uiadmin.feedback.FeedbackReplyAdminActivity;
+import com.example.fptparkingproject.uiadmin.feedback.ListFeedbackAdminActivity;
 import com.example.fptparkingproject.untils.Until;
 import com.google.gson.Gson;
 
@@ -49,18 +52,29 @@ public class ViewFeedBackAdminAdapter extends RecyclerView.Adapter<RecyclerView.
         ((FeedBackRowAdminViewHolder) holder).txtTitle.setText(feedback.getFeedbackTitle());
         ((FeedBackRowAdminViewHolder) holder).txtDatetime.setText(new Until().nomalizeDateTime(feedback.getFeedbackDateTime()));
         ((FeedBackRowAdminViewHolder) holder).txtContent.setText(feedback.getFeedbackContent());
-        ((FeedBackRowAdminViewHolder) holder).txtStatus.setText("1".equals(feedback.getFeedbackStatus()) ? R.string.feedback_not_approve : R.string.feedback_approve);
-
-        ((FeedBackRowAdminViewHolder) holder).setItemClickListener(new ItemClickListener() {
-            @Override
-            public void onClick(View view, int position, boolean isLongClick) {
-                Gson gson = new Gson();
-                Intent intent = new Intent(context, FeedbackReplyAdminActivity.class);
-                intent.putExtra(constant.INTENT_DETAIL_FEEDBACK, gson.toJson(listFeedbacks.get(position)));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                view.getContext().startActivity(intent);
-            }
-        });
+        if("1".equals(feedback.getFeedbackStatus())){
+            ((FeedBackRowAdminViewHolder) holder).txtStatus.setText(R.string.feedback_not_approve);
+            ((FeedBackRowAdminViewHolder) holder).txtStatus.setTextColor(Color.RED);
+            ((FeedBackRowAdminViewHolder) holder).setItemClickListener(new ItemClickListener() {
+                @Override
+                public void onClick(View view, int position, boolean isLongClick) {
+                    Gson gson = new Gson();
+                    Intent intent = new Intent(context, FeedbackReplyAdminActivity.class);
+                    intent.putExtra(constant.INTENT_DETAIL_FEEDBACK, gson.toJson(listFeedbacks.get(position)));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    view.getContext().startActivity(intent);
+                }
+            });
+        }else{
+            ((FeedBackRowAdminViewHolder) holder).txtStatus.setText(R.string.feedback_approve);
+            ((FeedBackRowAdminViewHolder) holder).txtStatus.setTextColor(Color.GREEN);
+            ((FeedBackRowAdminViewHolder) holder).setItemClickListener(new ItemClickListener() {
+                @Override
+                public void onClick(View view, int position, boolean isLongClick) {
+                    Toast.makeText(context, R.string.feedback_approved, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
     @Override
